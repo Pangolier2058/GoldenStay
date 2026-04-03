58ab74116fa02d49684b5e77cb3f60fa242d60f9
Ivan <chinchevich_is@mail.rum>
2026-04-03 10:46:49 +0500

    Очистка куки пользователя после выхода из системы.

--- logout.php
+++ logout.php
@@ -3,6 +3,12 @@
 
 session_start();
 $username = $_SESSION['username'] ?? 'Гость';
+
+// Очищаем cookie с данными формы бронирования
+if (isset($_COOKIE['booking_form'])) {
+    setcookie('booking_form', '', time() - 3600, '/');
+}
+
 session_destroy();
 session_start();
 $_SESSION['logout_message'] = 'До свидания, ' . htmlspecialchars($username) . '!';
